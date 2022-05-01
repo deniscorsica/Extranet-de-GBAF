93080684aad422aa197fee43ec5c789e055ef312
deniscorsica <61805354+deniscorsica@users.noreply.github.com>
2022-05-02 01:39:40 +0200

    Mentions légales
    Mentions légales

---
+++ mentionslegales.php
@@ -0,0 +1,66 @@
+<?php session_start();
+$title = 'Mentions légales';
+require("include/config.php");
+require_once("include/headerpublic.php");
+
+?>
+		
+			
+<div id="bloc_page">
+			<div id="mention">					
+				<h2>Mentions légales</h2>
+				
+<p>En vigueur au 25/04/2020<br />
+Formation développement Prép'fullstack Openclassrooms<br />
+Conformément aux dispositions des Articles 6-III et 19 de la Loi n°2004-575 du 
+21 juin 2004 pour la confiance dans l’économie numérique, dite L.C.E.N., il est 
+porté à la connaissance des utilisateurs et visiteurs, ci-après l'utilisateur 
+du site http://tnasdenis.local:81 , ci-après le "site", les présentes<br />
+mentions légales.<br />
+La connexion et la navigation sur le site par l’Utilisateur implique acceptation 
+intégrale et sans réserve des présentes mentions légales.<br />
+Ces dernières sont accessibles sur le Site à la rubrique « Mentions légales ».<br />
+<br />
+<strong>ARTICLE 1 - L'EDITEUR</strong><br />
+L’édition et la direction de la publication du Site est assurée par Drouin 
+denis, domiciliée Athis Mons , dont le numéro de téléphone est 0650830107, et 
+l'adresse e-mail <a href="mailto:drouindenis@bbox.fr">drouindenis@bbox.fr</a>. 
+ci-après l'éditeur".<br />
+<br />
+<strong>ARTICLE 2 - L'HEBERGEUR</strong><br />
+L'hébergeur du site est mon serveur nas, dont le siège social est situé au 
+Serveur Local ,<br />
+<br />
+<strong>ARTICLE 3 - ACCES AU SITE</strong><br />
+Le Site est accessible en tout endroit, 7j/7, 24h/24 sauf cas de force majeure, 
+interruption programmée ou non et pouvant découlant d’une nécessité de 
+maintenance.<br />
+En cas de modification, interruption ou suspension du Site, l'Editeur ne saurait 
+être tenu responsable.<br />
+<br />
+<strong>ARTICLE 4 - COLLECTE DES DONNEES<br />
+</strong>Le Site assure à l'Utilisateur une collecte et un traitement 
+d'informations personnelles dans le respect de la vie privée conformément à la 
+loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux 
+libertés.<br />
+En vertu de la loi Informatique et Libertés, en date du 6 janvier 1978, 
+l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et 
+d'opposition de ses données personnelles. <br />
+L'Utilisateur exerce ce droit :par mail à l'adresse email :
+<a href="mailto:drouindenis@bbox.fr">drouindenis@bbox.fr</a> via un formulaire 
+de contact via son espace personnel <br />
+Toute utilisation, reproduction, diffusion, commercialisation, modification de 
+toute ou partie du Site, sans autorisation de l’Editeur est prohibée et pourra 
+entraînée des actions et poursuites judiciairestelles que notamment prévues par 
+le Code de la propriété intellectuelle et le Code civil</p>
+
+					<div class ="retour">
+					<p> <strong>Retour à l'accueil</strong><br>
+						<button class="bouton_connexion" onclick= "window.location.href ='index.php';">Retour</button> 
+					</p>
+				</div>
+	 
+				</div>
+			</div>
+
+<?php require_once("include/footerpublic.php");?>
