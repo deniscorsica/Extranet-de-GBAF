c37bd6d3c9f2d7e360e5f35743e4a2cd02ef1873
deniscorsica <61805354+deniscorsica@users.noreply.github.com>
2022-05-03 00:29:40 +0200

    Mortification formulaire de contact
    Placeholder et correction des messages d'erreur

--- contact.php
+++ contact.php
@@ -1,26 +1,27 @@
 <?php
-require  "include/verifmail.php";
+require("include/config.php");
+require ("include/verifmail.php");
 // validation formulaire
    $civilite="Non Pénom";
-   @$nom=$_POST["nom"];
-   @$prenom=$_POST["prenom"];
-   @$email=$_POST["email"];
-   @$messages=$_POST["messages"];
-   @$valider=$_POST["valider"];
-   $email2= $email ;
+   $nom=htmlspecialchars($_POST["nom"]);
+   $prenom=htmlspecialchars($_POST["prenom"]);
+   $email=htmlspecialchars($_POST["email"]);
+   $messages=htmlspecialchars($_POST["messages"]);
+   $valider=$_POST["valider"];
+   
 
 // Gestion des errreus 
     if(isset($valider)){
       if(empty($nom))
-            $message='<div class="erreur"><h4>Nom obligatoire.<h4></div>';
+            $message='<div class="erreur"><h4>Veuillez remplir votre Nom<h4></div>';
       elseif(empty($prenom))
-         $message='<div class="erreur"><h4>Prénom oblogatoire.<h4></div>';
+         $message='<div class="erreur"><h4>Veuillez remplir votre Prénom<h4></div>';
       elseif(empty($email))
-         $message='<div class="erreur"><h4>E-mail laissé vide.<h4></div>';
-	  elseif(check_email_address($email2) == false)
+         $message='<div class="erreur"><h4>Veuillez remplir votre e-mail<h4></div>';
+	  elseif(check_email_address($email) == false)
 		 $message='<div class="erreur"><h4>E-mail non valide.<h4></div>';
 	  elseif(!strlen(trim($messages)))
-		  $message='<div class="erreur"><h4>Message obligatoire.<h4></div>';
+		  $message='<div class="erreur"><h4>Veuillez remplir votre message<h4></div>';
        else{
 		   // Resultat  du formulaire
          $message='<div class="rappel"><b>Rappel:</b><br />';
@@ -47,13 +48,13 @@ require_once("include/headerpublic.php");
 			<h2>Nous contacter</h2>
 			<p>Un problème, une question ? N’hésitez pas à utiliser ce formulaire pour prendre contact avec nous !</p>
 			<div class="form"> 
-			<form name="fo" method="post" action="">
+		<form name="fo" method="post" action="">
 			<div>Votre nom</div>
-			<input class="input" type="text" id="nom" name="nom"  value="<?php echo $nom?>">
+			<input class="input" type="text" id="nom" name="nom"  placeholder="Votre Nom" value="<?php echo $nom?>">
 			<div>Votre prénom</div>
-			<input class="input" type="text" id="prenom" name="prenom"  value="<?php echo $prenom?>"> 
+			<input class="input" type="text" id="prenom" name="prenom" placeholder="Votre Prénom"  value="<?php echo $prenom?>"> 
 			<div>Votre e-mail</div>
-			<input class="input" type="text" id="email" name="email" value="<?php echo $email?>">
+			<input class="input" type="text" id="email" name="email"  placeholder="Votre e-mail" value="<?php echo $email?>">
 			<div>Quel est le sujet de votre message ?</div>
 			<div class="select-wrapper"><select name="objet" id="objet"><div>
 			<option value="" disabled selected hidden>Choisissez le sujet de votre message</option>
@@ -61,10 +62,8 @@ require_once("include/headerpublic.php");
 			<option value="autre">Autre...</option>
 			</select>
 			<div><label for="message">Votre message</label></div>
-			<textarea id="messages" name="messages" placeholder="Bonjour, je vous contacte car...."> </textarea>
-			
-			
-		<button  class="bouton_connexion" type="submit" name="valider" value="Valider">Envoyer mon message</button>
+			<textarea id="messages" name="messages" placeholder="Votre message" ></textarea>
+			<button  class="bouton_connexion" type="submit" name="valider" value="Valider">Envoyer mon message</button>
 		</form>
 		
 		</div>	</div></div></div>
