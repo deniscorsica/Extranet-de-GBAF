1bff903f2e1f3dd32f2e01fe213c637c0a8ca4bd
denis <61805354+deniscorsica@users.noreply.github.com>
2022-05-10 11:04:37 +0200

    Delete verifmail.php

--- include/verifmail.php
+++
@@ -1,7 +0,0 @@
-<?php
-   
-// Foncion email Valide
-function check_email_address($email2) { 
-    return (!preg_match( "^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^", $email2)) ? false : true; 
-}
-?>
