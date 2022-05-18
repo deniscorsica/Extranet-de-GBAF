953006221300acccff88d1860d0a2638fc0201aa
denis <61805354+deniscorsica@users.noreply.github.com>
2022-05-18 14:09:23 +0200

    Ajout de la date en PHP

--- include/footer.php
+++ include/footer.php
@@ -1,18 +1,12 @@
 <!-- FOOTER -->     
-	    <footer>
-	    	<div class="shadowbox">
-	    	<p>Site créé dans le cadre du projet 3 Prép'fullstack 
-			<a class="cool"  href="https://openclassrooms.com/" target="_blank">OpenClassrooms</a>
-			
-			<!--DATE DU JOUR -->
-	 <script language="Javascript">
-			var maintenant=new Date();
-			var jour=maintenant.getDate();
-			var mois=maintenant.getMonth()+1;
-			var an=maintenant.getFullYear();
-			document.write("nous sommes le ",jour,"/",mois,"/",an,".");
-		</script></p>
+	<footer>
+		<div class="shadowbox">
+	  	<p>Site créé dans le cadre du projet 3 Prép'fullstack 
+		<a class="cool"  href="https://openclassrooms.com/" target="_blank">OpenClassrooms</a>
+		<!--DATE DU JOUR -->
+	 	nous sommes le <?php date_default_timezone_set('Europe/Paris'); $date = date('d-m-y'); echo $date;?>
+		</p>
 		</div>
-	    </footer>
-	</body>
+	  </footer>
+</body>
 </html> 
